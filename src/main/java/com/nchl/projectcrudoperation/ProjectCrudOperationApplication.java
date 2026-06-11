e2fb9344de031b642a45cfff6589899f87661d64
smritibk <smriti.bk201@gmail.com>
2026-06-11 12:11:15 +0545

    before tomcat

--- src/main/java/com/nchl/projectcrudoperation/ProjectCrudOperationApplication.java
+++ src/main/java/com/nchl/projectcrudoperation/ProjectCrudOperationApplication.java
@@ -2,9 +2,18 @@ package com.nchl.projectcrudoperation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import javax.swing.*;
 
 @SpringBootApplication
-public class ProjectCrudOperationApplication {
+public class ProjectCrudOperationApplication  {
+
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+//        return application.sources(ProjectCrudOperationApplication.class);
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectCrudOperationApplication.class, args);
